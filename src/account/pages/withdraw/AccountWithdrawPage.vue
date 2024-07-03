0bb10f962a44b53ab9ea7e4b334a270856b50198
Yoonseoparkk <patpark97@gmail.com>
2024-07-03 16:07:47 +0900

    feat: [박윤서] 회원 탈퇴 신청 페이지 추가 [EESV-46]

---
+++ src/account/pages/withdraw/AccountWithdrawPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <h2>회원 탈퇴 신청 페이지</h2>
+    </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+const accountModule = 'accountModule'
+
+export default {
+    data () {
+        return {
+        }
+    },
+}
+</script>
