c8e9ec2aa1c7c38f35fb6731ff886b5365a65a2c
Yoonseoparkk <patpark97@gmail.com>
2024-07-03 16:28:21 +0900

    feat: [박윤서] 구매 동향 데이터 분석 페이지 추가 [EESV-42]

---
+++ src/report/pages/purchaseTrendAnalysis/PurchaseTrendAnalysisPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+      <div class="title-section">
+        <text>상품 구매 동향 분석</text>
+      </div>
+    </v-container>
+</template>
+
+<script>
+// import * as d3 from 'd3';
+
+
+</script>
+
+<style>
+.title-section {
+    padding: 16px;
+    font-weight: bold;
+    font-size: 1.5rem;
+    text-align: left;
+    background-color: #fafafa;
+    border-bottom: 1px solid #eeeeee;
+  }
+</style>
