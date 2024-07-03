acc5ee2a62237d569e702c99daa1c363660edcdd
Yoonseoparkk <patpark97@gmail.com>
2024-07-03 16:29:58 +0900

    feat: [박윤서] 회원 이탈 예측 페이지 추가 [EESV-52]

--- src/navigationBar/NavigationMenuBar.vue
+++ src/navigationBar/NavigationMenuBar.vue
@@ -81,8 +81,8 @@ export default {
   data() {
     return {
       reportItems: [
-                { title: '구매 동향 분석', action: () => { this.goToHome() } },
-                { title: '회원 이탈 예측', action: () => { this.goToHome() } },
+                { title: '구매 동향 분석', action: () => { this.goToPurchaseTrendAnalysisPage() } },
+                { title: '회원 이탈 예측', action: () => { this.goToCustomerChurnPredictionPage() } },
       ],
       myPageItems: [
                 { title: '👤MY PAGE', action: () => { this.goToMyPage() } },
@@ -125,6 +125,12 @@ export default {
     },
     goToMyPage () {
         router.push('/account/mypage')
+    },
+    goToPurchaseTrendAnalysisPage () {
+        router.push('/report/purchase-trend-analysis')
+    },
+    goToCustomerChurnPredictionPage () {
+        router.push('/report/customer-churn-prediction')
     },
   },
   mounted() {
