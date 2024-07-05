5aa2362d6dabc76b06fff6b90087167a7a2421ab
Gomin <138251577+MinMessi@users.noreply.github.com>
2024-07-05 15:31:17 +0900

    feat: [민경원] 최초 카카오 인증 로그인 화면에서 성별과 출생년도를 입력받도록 구성합니다.[EESV-59]

--- src/account/pages/register/AccountRegisterPage.vue
+++ src/account/pages/register/AccountRegisterPage.vue
@@ -4,16 +4,20 @@
             <v-col cols="12" md="8" lg="6">
                 <v-card>
                     <v-card-title>
-                        <span class="headline">신규 회원 신청</span>
+                        <span class="headline">간편 신규 회원가입</span>
                     </v-card-title>
-                    <v-card-text>
+                    
+                    
+                    <v-responsive class="mx-auto" min-width="400">
                         <v-form ref="form" v-model="formValid" lazy-validation>
                             <v-text-field
                                     v-model="email"
                                     label="Email"
+                                    variant="solo"                                    
                                     required
                                     :rules="emailRules"
                                     :disabled="true"/>
+                            
                             <v-row align="center">
                                 <v-col cols="10">
                                     <v-text-field
@@ -23,23 +27,66 @@
                                             :rules="nicknameRules"
                                             :error-message="nicknameErrorMessages"/>
                                 </v-col>
+                                
                                 <v-col cols="2">
-                                    <v-btn color="black"
-                                            @click="checkNicknameDuplication"
-                                            class="check-button"
-                                            small>
-                                         중복 검사 
+                                    <v-btn
+                                        color="gb(200, 255, 0)"
+                                        style="color: black; width:90px"                                        
+                                        small
+                                        :disabled="
+                                            nickname == '' ||
+                                            isNicknameValid == true"
+                                        type="button"
+                                        @click="checkNicknameDuplication">
+                                        중복 검사
                                     </v-btn>
                                 </v-col>
                             </v-row>
+                            
+                            <div v-if="isNicknameValid" class="valid-nickname-box" style="color: chartreuse;">
+                                        사용 가능한 닉네임입니다.
+                            </div>
+
+                            <div style="margin-top: 32px;">
+                                <v-radio-group
+                                    v-model="gender"
+                                    inline
+                                    label="성별"
+                                    color="black">
+                                    <v-radio
+                                        label="남성"
+                                        value="man"
+                                        color="cyan"
+                                        base-color="white"
+                                    ></v-radio>
+                                    <v-radio
+                                        label="여성"
+                                        value="woman"
+                                        color="pink"
+                                        base-color="white"
+                                    ></v-radio>
+                                </v-radio-group>
+                            </div>
+
+                            
+
+                        <v-text-field
+                                v-model="birthyear"
+                                label="출생년도" 
+                                :rules="birthyearRules"
+                                required            
+                        />                       
+                        
                         </v-form>
-                    </v-card-text>
+                    </v-responsive>    
+                    
+
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn color="black" 
                                 @click="submitForm" 
                                 :disabled="!isValidForSubmission">
-                            신청하기
+                            회원가입 하기
                         </v-btn>
                     </v-card-actions>
                 </v-card>
@@ -65,8 +112,16 @@ export default {
                 v => /.+@.+\..+/.test(v) || '유효한 Email 주소를 입력하세요!'
             ],
             nicknameRules: [v => !!v || 'Nickname은 필수입니다!'],
+            // passwordRules: [v => !!v || 'Password는 필수입니다!'],
+            gender: "man",
+            birthyear: '',
             nicknameErrorMessage: [],
             isNicknameValid: false,
+            birthyearRules: [
+                v => !!v || '출생년도는 필수입니다!',
+                v => /^\d+$/.test(v) || '출생년도는 숫자여야 합니다!',
+                v => v.length === 4 || '출생년도는 4자리여야 합니다!',
+            ],
         }
     },
     async created () {
@@ -74,8 +129,8 @@ export default {
     },
     computed: {
         isValidForSubmission () {
-            return this.formValid && this.isNicknameValid
-        }
+            return this.formValid && this.isNicknameValid && this.age !== 0;
+        },
     },
     methods: {
         ...mapActions(authenticationModule, ['requestUserInfoToDjango', 'requestAddRedisAccessTokenToDjango']),
@@ -109,11 +164,14 @@ export default {
             }
         },
         async submitForm () {
-            console.log('신청하기 누름')
+            console.log('회원가입 하기 누름')
             if (this.$refs.form.validate()) {
                 const accountInfo = {
                     email: this.email,
                     nickname: this.nickname,
+                    // password: this.password,     // 비밀번호 추가
+                    gender: this.gender,            // 성별 추가
+                    birthyear: this.birthyear,      // 생년월일 추가
                 }
                 await this.requestCreateNewAccountToDjango(accountInfo)
                 console.log('전송한 데이터:', accountInfo)
@@ -128,3 +186,47 @@ export default {
     }
 }
 </script>
+
+
+
+<style scoped>
+.v-card {
+    position: relative;
+    z-index: 1;
+    background-color: rgb(0, 0, 0);
+    color: white;
+    min-width: 400px;
+    min-height: 500px;
+    padding: 20px;
+    border-top-width: 50px;    
+    border-radius: 20px;
+}
+.v-btn {
+    position: relative;
+    padding: 8px 16px;
+    margin-right: 10px;
+    background-color: rgb(200, 255, 0);
+}
+
+:deep(.v-radio-group > .v-input__control > .v-label) {
+    color: #fff !important;
+    margin-inline-start: 0px !important;
+    margin-bottom: 12px !important;
+    opacity: 1 !important;
+}
+
+:deep(
+        .v-radio-group
+            > .v-input__control
+            > .v-label
+            + .v-selection-control-group
+    ) {
+    padding-inline-start: 0px !important;
+    margin-top: 0px !important;
+}
+
+:deep(.v-messages__message) {
+    color: rgb(200, 255, 0)!important; 
+    font-size: 18px;
+}
+</style>
