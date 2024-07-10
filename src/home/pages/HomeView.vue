aa1a3d9dcd96141e9cc6c153f6fed68d305a33a1
moneyandjelly <rmsdldy12@gmail.com>
2024-07-10 11:31:48 +0900

    style: [이근] 홈 페이지 디자인 변경 [EESV-53]

--- src/home/pages/HomeView.vue
+++ src/home/pages/HomeView.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="home-container">
-    <!-- <div class="charts-container">
-      <div class="chart" ref="chart1"></div>
-      <div class="chart" ref="chart2"></div>
-      <div class="chart" ref="chart3"></div>
-    </div> -->
-
     <img :src="imageSrc" alt="Home Image" class="home-image" />
 
     <div class="text-container">
@@ -17,13 +11,13 @@
       </p>
       <div class="buttons">
         <button class="goToProduct-button" @click="goToProductList">
-          👟 러닝화 상품 구경하기
+          러닝화 상품 구경하기
         </button>
         <button class="goToCommunity-button" @click="goToCommunityList">
-          👨‍👩‍👧‍👦 커뮤니티 이동하기
+          커뮤니티 이동하기
         </button>
         <button class="goToReview-button" @click="goToReviewList">
-          ⭐ 별점 리뷰 달기
+          별점 리뷰 달기
         </button>
       </div>
     </div>
@@ -61,80 +55,20 @@ export default defineComponent({
     goToReviewList() {
       router.push("/review/list");
     },
-
-    // drawDonutChart(container, data, title) {
-    //   const width = 200;
-    //   const height = 200;
-    //   const radius = Math.min(width, height) / 2;
-    //   const color = d3.scaleOrdinal(['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#2ecc71', '#e74c3c']);
-
-    //   const svg = d3.select(container)
-    //     .append('svg')
-    //     .attr('width', width)
-    //     .attr('height', height)
-    //     .append('g')
-    //     .attr('transform', `translate(${width / 2}, ${height / 2})`);
-
-    //   const arc = d3.arc()
-    //     .innerRadius(radius - 50)
-    //     .outerRadius(radius);
-
-    //   const pie = d3.pie()
-    //     .value(d => d.value);
-
-    //   const path = svg.selectAll('path')
-    //     .data(pie(data))
-    //     .enter()
-    //     .append('path')
-    //     .attr('d', arc)
-    //     .attr('fill', (d, i) => color(i));
-
-    //   // 제목 추가
-    //   svg.append('text')
-    //     .attr('text-anchor', 'middle')
-    //     .attr('dy', '0.35em')
-    //     .style('font-size', '14px')
-    //     .style('font-weight', 'bold')
-    //     .style('fill', '#fff') // 폰트 색상 설정
-    //     .text(title);
-    // }
   },
-  // mounted() {
-  //   const data1 = [
-  //     { name: 'A', value: 30 },
-  //     { name: 'B', value: 70 }
-  //   ];
-
-  //   const data2 = [
-  //     { name: 'C', value: 40 },
-  //     { name: 'D', value: 60 }
-  //   ];
-
-  //   const data3 = [
-  //     { name: 'E', value: 50 },
-  //     { name: 'F', value: 50 }
-  //   ];
-
-  //   this.drawDonutChart(this.$refs.chart1, data1, '차트 1');
-  //   this.drawDonutChart(this.$refs.chart2, data2, '차트 2');
-  //   this.drawDonutChart(this.$refs.chart3, data3, '차트 3');
-  // }
 });
 </script>
 
 <style scoped>
 .home-container {
-  margin-top: 20px; /* 네비게이션 바와의 여유 공간 */
   text-align: center;
 }
 
 .home-image {
   width: 100%;
-  max-width: 1200px; /* 적절한 크기로 설정 */
   height: auto;
   margin: auto;
   display: block;
-  border-radius: 10px;
   object-fit: cover;
 }
 
